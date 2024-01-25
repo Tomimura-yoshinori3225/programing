9ecce62cb53b85f45b407dcf613e7aff759b3bf0
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2024-01-25 11:11:13 +0900

    .h達

--- Game/DriveAndAvoid/Scene/RankingInputScene.h
+++ Game/DriveAndAvoid/Scene/RankingInputScene.h
@@ -1,5 +1,11 @@
 #pragma once
-class RankingInputScene
+
+#include "SceneBase.h"
+#include "../object/RankingData.h"
+class RankingInputScene:public SceneBase
 {
+private:
+	int background_image;    //”wŒi‰æ‘œ
+	RankingData* ranking;    //ƒ‰ƒ“ƒLƒ“ƒOî•ñ
 };
 
