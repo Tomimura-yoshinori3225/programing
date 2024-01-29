da91e16ff6fb28ce644dd74bf274c556e009c1ca
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2024-01-29 15:01:07 +0900

    .hを書いている

--- Game/DriveAndAvoid/Scene/ResultScene.h
+++ Game/DriveAndAvoid/Scene/ResultScene.h
@@ -1,5 +1,23 @@
 #pragma once
-class ResultScene
+#include "SceneBase.h"
+class ResultScene:public SceneBase
 {
+private:
+	int back_groud;  //”wŒi‰æ‘œ
+	int score;       //ƒXƒRƒA
+	int enemy_count[3];    //“G‚ÌƒJƒEƒ“ƒg
+	int enemy_image[3];    //“G‰æ‘œ
+
+public:
+	ResultScene();
+	virtual ~ResultScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+private:
+	void ReadResultData();
 };
 
