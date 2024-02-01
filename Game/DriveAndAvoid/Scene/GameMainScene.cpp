d118dcb4738ed0a8319c51b4f3daca07eb135bfd
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2024-02-01 14:45:28 +0900

    .h＆。ｃｐｐ

--- Game/DriveAndAvoid/Scene/GameMainScene.cpp
+++ Game/DriveAndAvoid/Scene/GameMainScene.cpp
@@ -1,1 +1,8 @@
 #include "GameMainScene.h"
+#include "../Object/RankingData.h"
+#include "DxLib.h"
+#include <math.h>
+
+GameMainScene::GameMainScene() :high_score(0),back_ground(NULL),
+barrier_image(NULL),
+                                     mileage(0),player(nullptr),
--- Game/DriveAndAvoid/Scene/SceneManager.h
+++ Game/DriveAndAvoid/Scene/SceneManager.h
@@ -21,8 +21,11 @@ public:
 	void Update();                    //更新処理
 	void Finalize();                  //終了時処理
 
-privare: 
+private: 
 	void  Draw() const;             //描画処理
+	void ChangeScene(eSceneType scene_type);      //シーン切り替え処理
+	SceneBase* CreateScene(eSceneType scene_type);   //シーン生成処理
+
 
 };
 
