e64c869fb56051a136e19eff0e38a7b840781ee6
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2024-02-05 17:31:08 +0900

    .cpp

--- Game/DriveAndAvoid/Scene/ResultScene.cpp
+++ Game/DriveAndAvoid/Scene/ResultScene.cpp
@@ -3,4 +3,67 @@
 #include "../Utility/InputControl.h"
 #include "DxLib.h"
 
+ResultScene::ResultScene() :back_ground(NULL), score(0)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		enemy_image[i] = NULL;
+		enemy_count[i] = NULL;
+	}
+}
 
+ResultScene::~ResultScene()
+{
+
+}
+
+//初期化処理
+viod ResultScene::Initialize()
+{
+	//画像の読み込み
+	back_ground = LoadGraph("Resource/images/back.bmp");
+	int result = LoadDivGraph("Resource/images/car.bmp", 3, 3, 1, 63, 120,
+		enemy_image);
+
+	//エラーチェック
+	if (back_ground == -1)
+	{
+		throw("Resource/images/back.bmpがありません`n");
+	}
+	if (result == -1)
+	{
+		throw("Resource/images/car.bmpがありません`n");
+	}
+
+	//ゲーム結果の読み込み
+	ReadResultData();
+}
+
+//更新処理
+eSceneType ResultScene::Update()
+{
+	//Bボタンでランキングに遷移する
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_B))
+	{
+		return eSceneType::E_RANKING_INPUT;
+	}
+	return GetNowScene();
+}
+
+//描画処理
+void ResultScene::Draw() const
+{
+	//背景画像を描画
+	DrawGraph(0, 0, back_ground, TRUE);
+
+	//スコア等表示領域
+	DrawBox(150, 150, 490, 330, GetColor(0, 153, 0), TRUE);
+	DrawBox(150, 150, 490, 330, GetColor(0, 0, 0), FALSE);
+
+	DrawBox(500,0, 640, 480, GetColor(0, 153, 0), TRUE);
+
+	SetFontSize(20);
+	DrawString(220, 170, "ゲームオーバー", GetColor(204, 0, 0));
+	SetFontSize(16);
+	DrawString(180, 200, "走行距離    ",GetColor(0, 0, 0));
+}
